2470e64897e84d28d0e88efca3d4638ea4700251
Muratbekuli Meyirzhan <smilexah@inbox.ru>
2023-02-04 17:21:29 +0600

    first commit

---
+++ app/src/main.js
@@ -0,0 +1,30 @@
+import $, { data } from 'jquery';
+
+function getPageList() {
+    $('h1').remove();
+    $.get(
+        './api',
+        (data) => {
+            data.forEach((file) => {
+                $('body').append(`<h1>${file}</h1>`);
+            });
+        },
+        'JSON'
+    );
+}
+
+getPageList();
+
+$('button').click(() => {
+    $.post(
+        './api/createNewPage.php',
+        {
+            name: $('input').val(),
+        },
+        () => {
+            getPageList();
+        }
+    ).fail(() => {
+        alert('This webpage exists.');
+    });
+});
